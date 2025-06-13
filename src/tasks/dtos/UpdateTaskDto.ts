5d8011bc4cc5bb9213d4ddad699d081434261296
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-06-13 20:03:50 +0300

    Apply Validation on UpdateTaskDto

--- src/tasks/dtos/UpdateTaskDto.ts
+++ src/tasks/dtos/UpdateTaskDto.ts
@@ -1,11 +1,17 @@
 ﻿import { TaskStatus } from '../models/task.model';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 
 export class UpdateTaskDto {
   @ApiProperty()
+  @IsNotEmpty()
   title: string;
+
   @ApiProperty()
+  @IsNotEmpty()
   description: string;
+
   @ApiProperty()
+  @IsEnum(TaskStatus)
   status: TaskStatus;
 }
