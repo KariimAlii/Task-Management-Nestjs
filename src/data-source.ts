a8ee333094aae097abb3de58e4a2d423d1d63de8
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-07-05 01:48:20 +0300

    Fix: Data Source

--- src/data-source.ts
+++ src/data-source.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
   password: '1111',
   database: 'tasks-management-2',
   entities: [Task, User, Project], // Add all your entities
-  migrations: ['src/migrations/*.ts'], // Migration files location
+  migrations: ['src/migrations/*.js'], // Migration files location
   migrationsTableName: 'typeorm_migrations', // Optional custom name
   synchronize: false, // MUST BE FALSE when using migrations
   logging: true, // Enable SQL query logging (optional)
