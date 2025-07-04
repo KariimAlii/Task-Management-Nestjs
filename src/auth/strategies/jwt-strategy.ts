cd41d329819a31b71288c6c8ecb86ae4cb914c6f
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-07-05 02:49:11 +0300

    Use JWT Secret as Environment Variable

---
+++ src/auth/strategies/jwt-strategy.ts
@@ -0,0 +1,17 @@
+﻿import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { UserRepository } from '../repositories/user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  // constructor(private readonly userRepository: UserRepository) {
+  //   super({
+  //
+  //   });
+  // }
+  validate(...args: any[]): unknown {
+    throw new Error('Method not implemented.');
+  }
+}
