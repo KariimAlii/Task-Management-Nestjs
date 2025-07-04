501c527af8270efa3b5351a1b68b95cb1590fe01
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-07-04 23:38:17 +0300

    User Repository needs to be injectable

--- src/auth/repositories/user.repository.ts
+++ src/auth/repositories/user.repository.ts
@@ -1,7 +1,8 @@
 ﻿import { DataSource, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
+import { Injectable } from '@nestjs/common';
 
-
+@Injectable()
 export class UserRepository {
   private repo: Repository<User>;
   constructor(private readonly dataSource: DataSource) {
